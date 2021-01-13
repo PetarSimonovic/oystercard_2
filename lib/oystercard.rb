ed340468fe9d4ccd88f52d5b389d170c26311701
twigz826 <thomastwigden@gmail.com>
2021-01-13 13:28:52 -0300

    Halfway through Chapter 12

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,55 +1,51 @@
 class OysterCard
 
-   CARD_LIMIT = 90
-   MINIMUM_FARE = 1
+  CARD_LIMIT = 90
+  MINIMUM_FARE = 1
 
-    attr_reader :balance, :entry_station
-    attr_accessor :active
+  attr_reader :balance, :entry_station, :journey_history
+  attr_accessor :active
 
-    private
-    attr_writer :balance
+  private
+  attr_writer :balance
 
-    public
+  public
+  def initialize
+    @journey_history = {}
+    @balance = 0
+    @entry_station = false
+  end
 
-    def initialize
-        @balance = 0
-        @active = false
-        @entry_station = nil
-    end
+  def top_up(amount)
+    fail "Card limit of £#{CARD_LIMIT} reached" if limit_exceeded?(amount)
+    @balance += amount
+  end
 
-    def top_up(amount)
-      fail "Card limit of £#{CARD_LIMIT} reached" if limit_exceeded?(amount)
-      @balance += amount
-    end
+  def deduct(amount)
+    @balance -= amount
+  end
 
-    def deduct(amount)
-      @balance -= amount
-    end
+  def touch_in(station)
+    fail "Not enough money on card" if @balance < MINIMUM_FARE
+    @entry_station = station
+    @journey_history[:entry_station] = @entry_station
+  end
 
-    def touch_in(station)
-      fail "Not enough money on card" if @balance < MINIMUM_FARE
+  def touch_out(station)
+    @entry_station = nil
+    deduct(amount = MINIMUM_FARE)
+  end
 
-      @entry_station = station
-      @active = true
-    end
+  def in_journey?
+    @entry_station
+  end
 
-    def touch_out
-      @active = false
-      @entry_station = nil 
-      deduct(amount = MINIMUM_FARE)
-    end
+  def balance_check
+  end
 
-    def in_journey?
-      @active
-    end
-
-    def balance_check
-    end
-
-    private
-
-    def limit_exceeded?(amount)
-      balance + amount > CARD_LIMIT
-    end
+  private
+  def limit_exceeded?(amount)
+    balance + amount > CARD_LIMIT
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,79 +2,82 @@ require 'oystercard'
 
 describe OysterCard do
 
-    let(:station) { double :station }
+  let(:station) { double :station }
+  let(:station_out) { double :station_out }
 
-    it { is_expected.to respond_to(:balance) }
+  describe '#top_up without min fare' do
 
-    describe 'initialization' do
+    it "does not touch in unless balance has minimum fare" do
+      card = OysterCard.new
+      expect{ card.touch_in(station) }.to raise_error "Not enough money on card"
+    end
+  end
 
-        it 'has a numeric balance' do
-            expect(subject.balance).to be_an(Numeric)
-        end
+  before do
+    subject.top_up(10)
+  end
 
+  it { is_expected.to respond_to(:balance) }
+
+  describe 'initialization' do
+
+    it 'has a numeric balance' do
+      expect(subject.balance).to be_an(Numeric)
     end
 
-    describe '#top_up' do
+  end
 
-        it 'increases the balance with the amount passed as an argument' do
-            subject.top_up(15)
-            expect(subject.balance).to eq 15
-        end
-
-        it "raises an exception when the new balance exceeds the limit" do
-          card = OysterCard.new
-          card.top_up(OysterCard::CARD_LIMIT)
-          expect { card.top_up(1) }.to raise_error "Card limit of £#{OysterCard::CARD_LIMIT} reached"
-        end
+  describe '#top_up' do
+    it 'increases the balance with the amount passed as an argument' do
+      subject.top_up(5)
+      expect(subject.balance).to eq 15
     end
 
-    describe 'deduct' do
+    it "raises an exception when the new balance exceeds the limit" do
+      card = OysterCard.new
+      card.top_up(OysterCard::CARD_LIMIT)
+      expect { card.top_up(1) }.to raise_error "Card limit of £#{OysterCard::CARD_LIMIT} reached"
+    end
+  end
 
-      it 'deducts amount from the balance of the card' do
-        subject.top_up(15)
-        expect{ subject.deduct(5) }.to change{ subject.balance}.by -5
-      end
+  describe 'deduct' do
+
+    it 'deducts amount from the balance of the card' do
+      expect{ subject.deduct(5) }.to change{ subject.balance}.by -5
+    end
+  end
+
+  describe 'touch in' do
+
+    it { is_expected.to respond_to(:touch_in).with(1).argument }
+
+    it "remembers the entry station" do
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq station
     end
 
-    describe 'touch in' do
-
-      it { is_expected.to respond_to(:touch_in).with(1).argument }
-
-      it "changes active to true" do
-        subject.top_up(5)
-        subject.touch_in(station)
-        expect(subject.active).to eq true
-      end
-
-      it "does not touch in unless balance has minimum fare" do
-        expect{ subject.touch_in(station) }.to raise_error "Not enough money on card"
-      end
-
-      it "remembers the entry station" do
-        subject.top_up(5)
-        subject.touch_in(station)
-        expect(subject.entry_station).to eq station
-      end
-
+    it "stores the entry_station after touch in" do
+      subject.touch_in("Cockfosters")
+      expect(subject.journey_history).to include(:entry_station => "Cockfosters")
     end
 
-    describe ' touch out' do
+  end
 
-      it 'changes active to false' do
-        subject.top_up(5)
-        subject.touch_in(station)
-        subject.touch_out
-        expect(subject.active).to eq false
+  describe 'touch out' do
+    before do
+      subject.touch_in(station)
+    end
+
+    it 'responds to one argument' do
+      expect(subject).to respond_to(:touch_out).with(1).argument
     end
 
     it 'it deduct the minimum fare after touch out' do
-      subject.top_up(5)
-      subject.touch_in(station)
-      expect { subject.touch_out }.to change{ subject.balance }.by(-1)
+      expect { subject.touch_out(station_out) }.to change{ subject.balance }.by(-1)
     end
 
     it "forgets the entry_station at touch out" do
-      subject.touch_out
+      subject.touch_out(station_out)
       expect(subject.entry_station).to be nil
     end
 
@@ -83,17 +86,14 @@ describe OysterCard do
   describe 'states if in journey or not' do
 
     it "it is in journey after touch in" do
-      subject.top_up(5)
       subject.touch_in(station)
       expect(subject).to be_in_journey
     end
 
     it "it is not in journey after touch out" do
-      subject.top_up(5)
       subject.touch_in(station)
-      subject.touch_out
+      subject.touch_out(station_out)
       expect(subject).not_to be_in_journey
     end
   end
-
 end
